39b28df0951f50b80c841073a27039c71325c3d3
doston <dostonadxamov222@gmail.com>
2025-06-27 13:19:05 +0500

    ibitialcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/footer";
 import Headers from "./components/Headers";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 
 function App() {
   return (
